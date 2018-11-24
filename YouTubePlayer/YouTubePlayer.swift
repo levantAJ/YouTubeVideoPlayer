aea5d8a0c2f289782f4edb57d64e772b6333fbce
levantAJ <sirlevantai@gmail.com>
2018-11-24 16:37:52 +0700

    Adds podspec file

---
+++ YouTubePlayer/YouTubePlayer.swift
@@ -0,0 +1,14 @@
+//
+//  YouTubePlayer.swift
+//  YouTubePlayer
+//
+//  Created by levantAJ on 24/11/18.
+//  Copyright © 2018 levantAJ. All rights reserved.
+//
+
+import UIKit
+
+open class YouTubePlayer: UIView {
+
+
+}
