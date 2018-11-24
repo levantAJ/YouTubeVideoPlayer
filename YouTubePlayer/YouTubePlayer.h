96ee2c01eec29611e07a96358ed2e2a4da1f9d11
levantAJ <sirlevantai@gmail.com>
2018-11-24 16:32:12 +0700

    Creates lib

---
+++ YouTubePlayer/YouTubePlayer.h
@@ -0,0 +1,19 @@
+//
+//  YouTubePlayer.h
+//  YouTubePlayer
+//
+//  Created by levantAJ on 24/11/18.
+//  Copyright © 2018 levantAJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YouTubePlayer.
+FOUNDATION_EXPORT double YouTubePlayerVersionNumber;
+
+//! Project version string for YouTubePlayer.
+FOUNDATION_EXPORT const unsigned char YouTubePlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YouTubePlayer/PublicHeader.h>
+
+
